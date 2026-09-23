d432a724f4a396050be2eb16d91ea606cf88b125
Ale Pereira <naitwa-alexandre@gmail.com>
2026-09-23 07:29:42 -0300

    feat: creates attributes and checked on debug if it works, removes IKFoot logs

--- Source/Crunch/Public/GAS/Attributes/CrunchAttributeSet.h
+++ Source/Crunch/Public/GAS/Attributes/CrunchAttributeSet.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "CrunchAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+ GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+ GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+ GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+ GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,4 +20,20 @@ UCLASS()
 class CRUNCH_API UCrunchAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+	
+public:
+	ATTRIBUTE_ACCESSORS(UCrunchAttributeSet, Health);
+	ATTRIBUTE_ACCESSORS(UCrunchAttributeSet, MaxHealth);
+	ATTRIBUTE_ACCESSORS(UCrunchAttributeSet, Mana);
+	ATTRIBUTE_ACCESSORS(UCrunchAttributeSet, MaxMana);
+	
+private:
+	UPROPERTY()
+	FGameplayAttributeData Health;
+	UPROPERTY()
+	FGameplayAttributeData MaxHealth;
+	UPROPERTY()
+	FGameplayAttributeData Mana;
+	UPROPERTY()
+	FGameplayAttributeData MaxMana;
 };
