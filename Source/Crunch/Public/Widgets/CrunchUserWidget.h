8ad67cf1e53ab07b42b03bc3070b5a7d23dbd4a4
Ale Pereira <naitwa-alexandre@gmail.com>
2026-09-25 10:10:21 -0300

    feat: craeted widget base and value gauge document

---
+++ Source/Crunch/Public/Widgets/CrunchUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CrunchUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API UCrunchUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Crunch/Public/Widgets/UI/ValueGauge.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/CrunchUserWidget.h"
+#include "ValueGauge.generated.h"
+
+class UProgressBar;
+class UTextBlock;
+
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API UValueGauge : public UCrunchUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	void SetValue(float NewValue, float NewMaxValue) const;
+
+protected:
+	virtual void NativeConstruct() override;
+	
+private:
+	UPROPERTY(EditAnywhere, Category = "Visual")
+	FLinearColor BarColor;
+	
+	UPROPERTY(VisibleAnywhere, meta=(BindWidget))
+	UProgressBar* ProgressBar;
+	
+	UPROPERTY(VisibleAnywhere, meta=(BindWidget))
+	UTextBlock* ValueText;
+};
